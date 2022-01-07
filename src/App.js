98ee51dcc5ed3f5cd06f2441690eed1d46d40df9
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-07 17:21:53 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import  React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 title="This is the Heading">Heading - JSX Method</h1>
+      <p id="para1">This is a paragraph</p>
+     <p className="para2">This is a second paragraph</p>
+     <div>
+       <label htmlFor="input1">Some Data</label>
+       <input id="input1" name="input1"/>
+       <br/>
+      </div>
+      <br/>
+        <div>Hello World!</div>
+        <div>Text after width</div>
     </div>
   );
 }
